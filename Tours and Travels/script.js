2ab8287db3cdf93e3ba8c60609778e47f40a4d29
Pawan-Gautam11 <pawangautam11op@gmail.com>
2023-06-29 22:59:58 +0545

    Done

--- Tours and Travels/script.js
+++ Tours and Travels/script.js
@@ -4,10 +4,10 @@ window.addEventListener("scroll",function(){
     header.classList.toggle("sticky",this.window.scrollY > 0)
 });
 
-let menu=document.querySelector('#menu-icon');
-let navmenu=document.querySelector('.navmenu');
+let menu = document.querySelector('#menu-icon');
+let navmenu = document.querySelector('.navmenu');
 
-menu.onclick = ()=> {
+menu.onclick = () => {
     menu.classList.toggle('bx-x');
-    navmenu.classList.toggle('bx-x');
+    navmenu.classList.toggle('open');
 }
